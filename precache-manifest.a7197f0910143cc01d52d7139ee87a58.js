8019199cabe271fd0d640e5a8595832a19693242
vladistick <vladistick@gmail.com>
2020-09-23 18:42:26 +0200

    Updates

---
+++ precache-manifest.a7197f0910143cc01d52d7139ee87a58.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81be7bb594d9cec7c3be70fab96b0570",
+    "url": "/foodwars/index.html"
+  },
+  {
+    "revision": "6eb052a188505d4c9380",
+    "url": "/foodwars/static/css/2.99a5358a.chunk.css"
+  },
+  {
+    "revision": "cf8b85f5b35ba15bf1df",
+    "url": "/foodwars/static/css/main.7c99f0f6.chunk.css"
+  },
+  {
+    "revision": "6eb052a188505d4c9380",
+    "url": "/foodwars/static/js/2.582828e0.chunk.js"
+  },
+  {
+    "revision": "0008cd29374f8d4bc910711b22b68abd",
+    "url": "/foodwars/static/js/2.582828e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf8b85f5b35ba15bf1df",
+    "url": "/foodwars/static/js/main.e3f91536.chunk.js"
+  },
+  {
+    "revision": "3bfcafb81995e2cdfee0",
+    "url": "/foodwars/static/js/runtime-main.8422b8c8.js"
+  },
+  {
+    "revision": "ef8116aa432865ff38b764c7f5bb8af1",
+    "url": "/foodwars/static/media/pierogi.ef8116aa.png"
+  },
+  {
+    "revision": "dcd32a972d89a6e4bfdc7e46869a86b8",
+    "url": "/foodwars/static/media/pizza.dcd32a97.png"
+  },
+  {
+    "revision": "797984182dad8c3df51944600fc39328",
+    "url": "/foodwars/static/media/vs.79798418.png"
+  },
+  {
+    "revision": "a0ed865482e1af2e6994019a7ce469a4",
+    "url": "/foodwars/static/media/whole.a0ed8654.png"
+  }
+]);
